734b64c4829efa222ae0df182e27b03530b26fa5
totsuka <techstart35@gmail.com>
2023-10-13 14:55:05 +0900

    fix

--- main.go
+++ main.go
@@ -62,13 +62,15 @@ func main() {
 		},
 		// 許可したいHTTPリクエストヘッダ
 		AllowHeaders: []string{
-			"Access-Control-Allow-Credentials",
-			"Access-Control-Allow-Headers",
-			"Access-Control-Allow-Origin",
+			"Origin",
+			"Content-Length",
 			"Content-Type",
 			"Authorization",
+			"Accept",
+			"X-Requested-With",
 			"Passcode",
 		},
+		ExposeHeaders: []string{"Content-Length"},
 		// cookieなどの情報を必要とするかどうか
 		AllowCredentials: true,
 		// preflightリクエストの結果をキャッシュする時間
